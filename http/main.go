66418b7f6d8ceff0e0b0db7feedd2aa93c34ea60
Brendan Burns <bburns@microsoft.com>
2023-05-25 15:08:35 +0000

    Add proxy generated code.

--- http/main.go
+++ http/main.go
@@ -2,25 +2,54 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/dev-wasm/dev-wasm-go/proxy"
 )
 
 func main() {
-	response, err := Request(
-		"https://postman-echo.com/get",
-		"GET", 
-		"Content-type: text/html\nUser-agent: wasm32-wasi-http", 
-		"")
-	defer response.Close()
+	strstr := []proxy.TypesTuple2StringStringT{
+		{"User-agent", "WASI-HTTP-Go/0.0.1"},
+		{"Content-type", "application/json"},
+	}
+	headers := proxy.TypesNewFields(strstr)
 
-	if err != nil {
-		fmt.Printf("Request error: (%v)\n", err)
-		return
+	method := proxy.TypesMethodGet()
+	scheme := proxy.Some(proxy.TypesSchemeHttps())
+
+	path := "/get"
+	authority := "postman-echo.com"
+	query := ""
+
+	req := proxy.TypesNewOutgoingRequest(method, path, query, scheme, authority, headers)
+	var opts proxy.Option[proxy.TypesRequestOptions]
+	opts = proxy.None[proxy.TypesRequestOptions]()
+	res := proxy.DefaultOutgoingHttpHandle(req, opts)
+
+	resultOption := proxy.TypesFutureIncomingResponseGet(res)
+	if !resultOption.IsSome() {
+		log.Fatalf("No result!")
 	}
-	fmt.Printf("Request status: %v\n", response.StatusCode)
-	body, err := response.Body()
-	if err != nil {
-		fmt.Printf("Request error: (%v)\n", err)
-		return
+	result := resultOption.Unwrap().Unwrap()
+
+	code := proxy.TypesIncomingResponseStatus(result)
+
+	fmt.Printf("Status is %v\n", code)
+
+	responseHeaders := proxy.TypesIncomingResponseHeaders(result)
+	entries := proxy.TypesFieldsEntries(responseHeaders)
+
+	for _, entry := range entries {
+		fmt.Printf("%s: %s\n", entry.F0, entry.F1)
 	}
-	fmt.Printf("%v\n", string(body));
+
+	stream := proxy.TypesIncomingResponseConsume(result).Unwrap()
+
+	data := proxy.StreamsRead(stream, 64*1024).Unwrap()
+
+	fmt.Printf("%s\n", string(data.F0))
+
+	proxy.TypesDropOutgoingRequest(req)
+	proxy.StreamsDropInputStream(stream)
+	proxy.TypesDropIncomingResponse(result)
 }
